cd3943c01b3fbd242bb7c4a28171ea39c742abfd
Egor Kosmachev <egor_kosmachev.2003@mail.ru>
2024-02-19 11:43:08 +0300

    home-view

---
+++ public/js/sidebar.js
@@ -0,0 +1,20 @@
+window.onload = function(){
+    const sidebar = document.querySelector(".sidebar")
+    const closeBtn = document.querySelector("#btn")
+    const section = document.querySelector(".section")
+    closeBtn.addEventListener("click",function(){
+        sidebar.classList.toggle("open")
+        menuBtnChange()
+    })
+
+    function menuBtnChange(){
+        if(sidebar.classList.contains("open")){
+            closeBtn.classList.replace("bx-menu","bx-menu-alt-right")
+            section.style['padding-left'] = "250px"
+
+        }else{
+            closeBtn.classList.replace("bx-menu-alt-right","bx-menu")
+            section.style['padding-left'] = "75px"
+        }
+    }
+}
---
+++ resources/views/layouts/home/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    @vite([
+        'resources/sass/app.scss',
+        'resources/js/app.js',
+        'public/css/sidebar.css',
+        'public/js/sidebar.js',
+        'public/css/home/style.css'
+        ])
+    <title>Laravel_Project</title>
+</head>
+<body>
+    <!--SideBar-->
+    @include('includes.home.sidebar')
+    <div class='section'>
+        @yield('content')
+    </div>
+</body>
+</html>
