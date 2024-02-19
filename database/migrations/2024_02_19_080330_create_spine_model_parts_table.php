a9b3fa3d54052da994fba813c30f418c7f97615e
Egor Kosmachev <egor_kosmachev.2003@mail.ru>
2024-02-19 11:04:48 +0300

    spine-model-part-migration

---
+++ database/migrations/2024_02_19_080330_create_spine_model_parts_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('spine_model_parts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->unsignedBigInteger('spine_model_id');
+            $table->unsignedBigInteger('spine_part_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('spine_model_parts');
+    }
+};
